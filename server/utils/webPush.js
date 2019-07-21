182510aa92f1657b7f0ce5d8c15b7cc6403a30b4
charisTheo <haristh@hotmail.com>
2019-07-21 21:28:18 +0300

    fix keys.json error while building on heroku.

--- server/utils/webPush.js
+++ server/utils/webPush.js
@@ -1,15 +1,21 @@
 const webPush = require('web-push');
-const keys = require('../keys.json');
+const PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY;
 
-webPush.keys = {
-  PUBLIC_VAPID_KEY: process.env.PUBLIC_VAPID_KEY || keys.PUBLIC_VAPID_KEY,
-  PRIVATE_VAPID_KEY: process.env.PRIVATE_VAPID_KEY || keys.PRIVATE_VAPID_KEY
+if (process.env.NODE_ENV === 'dev') {
+  const keys = require('../keys.json');
+  PUBLIC_VAPID_KEY = keys.PUBLIC_VAPID_KEY;
+  PRIVATE_VAPID_KEY = keys.PRIVATE_VAPID_KEY;
+  
+} else {
+  PUBLIC_VAPID_KEY = process.env.PUBLIC_VAPID_KEY;
+  PRIVATE_VAPID_KEY = process.env.PRIVATE_VAPID_KEY;
+
 }
 
 webPush.setVapidDetails(
   'mailto:dev@charistheo.io',
-  webPush.keys.PUBLIC_VAPID_KEY,
-  webPush.keys.PRIVATE_VAPID_KEY
+  PUBLIC_VAPID_KEY,
+  PRIVATE_VAPID_KEY
 );
 
 module.exports = {webPush}
