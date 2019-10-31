cbb69e1db16d19e15b0bca31e1897a64a69beabc
Charis Theo <haristh@hotmail.com>
2019-10-31 15:41:16 +0000

    update iOS and standalone display mode checking functions

--- public/js/util.js
+++ public/js/util.js
@@ -1,11 +1,10 @@
 // Detects if device is on iOS 
 const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test( userAgent );
+  return /iPad|iPhone|iPod/.test(navigator.userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
 }
 
 // Detects if device is in standalone mode
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+const isInStandaloneMode = () => (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true);
 
 function urlBase64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
